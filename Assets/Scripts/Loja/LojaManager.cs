7886fd5aa77cb6056541280aba381a87631794f9
Pedro Luiz <pedrin.luiz22@gmail.com>
2026-08-03 20:13:02 -0300

    alguns ajuste e mudança de sprites

--- Assets/Scripts/Loja/LojaManager.cs
+++ Assets/Scripts/Loja/LojaManager.cs
@@ -11,18 +11,17 @@ public class LojaManager : MonoBehaviour
     public GameObject PanelLoja;
 
     [Header("Prateleira (Geração Dinâmica)")]
-    public Transform conteinerDaPrateleira; // Onde os botões vão nascer
-    public GameObject prefabBotaoAlimento;  // O botão vazio que serve de molde
+    public Transform conteinerDaPrateleira; 
+    public GameObject prefabBotaoAlimento; 
 
     [Header("Carrinho Visual")]
-    public Transform conteinerDoCarrinho;   // Onde os sprites das frutas vão nascer
-    public GameObject prefabIconeCarrinho;  // Apenas um molde de Image vazia
+    public Transform conteinerDoCarrinho;  
+    public GameObject prefabIconeCarrinho; 
     public TextMeshProUGUI textoTotalMoedas;
 
     private AlimentoSO _alimentoNoPopupAtual;
     private int _valorTotalCarrinho = 0;
 
-    // Lista para sabermos exatamente o que a criança comprou no final
     private List<AlimentoSO> _itensNoCarrinho = new List<AlimentoSO>();
 
     private void Start()
@@ -43,13 +42,11 @@ public class LojaManager : MonoBehaviour
 
     private void GerarPrateleirasDinamicamente()
     {
-        // 1. Limpa a prateleira por segurança (caso tenha botões de teste)
         foreach (Transform filho in conteinerDaPrateleira)
         {
             Destroy(filho.gameObject);
         }
 
-        // 2. Cria um botão para cada alimento que você cadastrou no catálogo!
         foreach (AlimentoSO alimento in catalogoDeAlimentos)
         {
             GameObject novoBotao = Instantiate(prefabBotaoAlimento, conteinerDaPrateleira);
@@ -59,25 +56,20 @@ public class LojaManager : MonoBehaviour
         }
     }
 
-    // Deixei essa função preparada para quando formos fazer o Popup!
     public void PrepararPopup(AlimentoSO alimentoClicado)
     {
         _alimentoNoPopupAtual = alimentoClicado;
         Debug.Log("Abrindo popup para: " + alimentoClicado.nomeAlimento);
 
-        // TODO: Ativar o painel do Popup e preencher os textos (Próxima etapa!)
     }
 
-    // O Botão de "Adicionar" dentro do Popup vai chamar esta função
     public void AdicionarAoCarrinho()
     {
         if (_alimentoNoPopupAtual == null) return;
 
-        // 1. Salva na lista de compras
         _itensNoCarrinho.Add(_alimentoNoPopupAtual);
         _valorTotalCarrinho += _alimentoNoPopupAtual.precoMoedas;
 
-        // 2. Cria o Sprite do alimento fisicamente lá na barra de baixo!
         GameObject iconeVisual = Instantiate(prefabIconeCarrinho, conteinerDoCarrinho);
         iconeVisual.GetComponent<Image>().sprite = _alimentoNoPopupAtual.iconeVisual;
 
@@ -89,7 +81,6 @@ public class LojaManager : MonoBehaviour
         textoTotalMoedas.text = _valorTotalCarrinho.ToString();
     }
 
-    // O Botão de "Joinha" (Thumbs Up) chama esta função
     public void BotaoComprar()
     {
         if (_itensNoCarrinho.Count == 0) return;
@@ -98,7 +89,6 @@ public class LojaManager : MonoBehaviour
         {
             Debug.Log("Compra Aprovada! Itens indo para a cozinha...");
 
-            // Aqui futuramente enviaremos para o PlayerPrefs da cozinha
 
             EsvaziarCarrinho();
         }
@@ -113,7 +103,6 @@ public class LojaManager : MonoBehaviour
         _itensNoCarrinho.Clear();
         _valorTotalCarrinho = 0;
 
-        // Destrói as imagens da barra de baixo
         foreach (Transform filho in conteinerDoCarrinho)
         {
             Destroy(filho.gameObject);
